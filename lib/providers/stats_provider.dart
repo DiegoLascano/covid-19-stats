51f076e976f6d2babff30b1361996aa64aa787f1
Diego Lascano <diegolascanodalgo@gmail.com>
2020-03-21 14:39:07 -0500

    google-service.json added to gitignore. Version changed in pubspec. Duplicated line in gradle file deleted.

--- lib/providers/stats_provider.dart
+++ lib/providers/stats_provider.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+// import 'package:translator/translator.dart';
 
 import 'package:covid_19_stats/models/global_model.dart';
 import 'package:covid_19_stats/models/country_model.dart';
@@ -7,6 +8,8 @@ import 'package:covid_19_stats/models/country_model.dart';
 class StatsProvider {
   String globalUrl = 'https://coronavirus-19-api.herokuapp.com/all';
   String byCountryUrl = 'https://coronavirus-19-api.herokuapp.com/countries';
+
+  // String _nameES = '';
 
   Future getGlobalStats() async {
     final response = await http.get(globalUrl);
@@ -21,8 +24,19 @@ class StatsProvider {
     final decodedData = json.decode(response.body);
     final countriesStats = new Countries.fromJsonList(decodedData);
 
+    // final perCountryStats = _translateNames(countriesStats.items);
+    // return perCountryStats;
+
     return countriesStats.items;
   }
+
+  // Future<List<Country>> _translateNames(List<Country> perCountryStats) async {
+  //   for (var i = 0; i < perCountryStats.length; i++) {
+  //     await _translateCountryName(perCountryStats[i].country);
+  //     perCountryStats[i].country = _nameES;
+  //   }
+  //   return perCountryStats;
+  // }
 
   Future<List<Country>> searchCountry(String query) async {
     final response = await http.get(byCountryUrl);
@@ -42,4 +56,13 @@ class StatsProvider {
 
     return countriesStats.items;
   }
+
+  // Future<String> _translateCountryName(String name) async {
+  //   final translator = new GoogleTranslator();
+  //   translator.translate(name, from: 'en', to: 'es').then((s) {
+  //     _nameES = s;
+  //     print(s);
+  //   });
+  //   return null;
+  // }
 }
